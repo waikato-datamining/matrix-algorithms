df14804b6227b8557b2a8da9ef843cbc6b2c11bf
Steven Lang <slang03@students.uni-mainz.de>
2018-07-31 15:03:28 +0200

    Extend YGradientGLSW from GLSW

--- src/main/java/com/github/waikatodatamining/matrix/algorithm/YGradientGLSW.java
+++ src/main/java/com/github/waikatodatamining/matrix/algorithm/YGradientGLSW.java
@@ -21,11 +21,13 @@ import static com.github.waikatodatamining.matrix.core.MatrixHelper.inv;
  *
  * @author Steven Lang
  */
-public class YGradientGLSW extends AbstractSingleResponsePLS {
+public class YGradientGLSW extends GLSW {
 
 
   /** Five Point Savitzky Golay Filter coefficients */
   protected static final double[] m_Coef = {2.0 / 10.0, 1.0 / 10.0, 0.0, -1.0 / 10.0, -2.0 / 10.0};
+
+  private static final long serialVersionUID = 4080767826836437539L;
 
   /** Savitzky Golay Filter constant h */
   protected double m_H;
@@ -34,71 +36,6 @@ public class YGradientGLSW extends AbstractSingleResponsePLS {
   protected void initialize() {
     super.initialize();
     m_H = 0.1;
-  }
-
-  @Override
-  protected void reset() {
-    super.reset();
-  }
-
-  @Override
-  public String[] getMatrixNames() {
-    return new String[0];
-  }
-
-  @Override
-  public Matrix getMatrix(String name) {
-    return null;
-  }
-
-  @Override
-  public boolean hasLoadings() {
-    return false;
-  }
-
-  @Override
-  public Matrix getLoadings() {
-    return null;
-  }
-
-  @Override
-  public boolean canPredict() {
-    return false;
-  }
-
-  @Override
-  protected Matrix doTransform(Matrix predictors) throws Exception {
-    return null;
-  }
-
-  @Override
-  protected String doPerformInitialization(Matrix predictors, Matrix response) throws Exception {
-    // Sort predictors and response in increasing Y value
-    Matrix X = predictors;
-    Matrix y = response;
-
-
-    double[] yVals = y.toRawCopy1D();
-    int[] sortedRowIndices = IntStream
-      .range(0, yVals.length)
-      .boxed()
-      .sorted(Comparator.comparingDouble(o -> yVals[o]))
-      .mapToInt(i -> i)
-      .toArray();
-
-    // Increasing/Decreasing??????
-    int[] allCols = IntStream.range(0, X.numColumns()).toArray();
-    Matrix Xsorted = X.getSubMatrix(sortedRowIndices, allCols);
-    Matrix ysorted = y.getSubMatrix(sortedRowIndices, new int[]{0});
-
-
-    // Apply 5-Point Savitzky–Golay filter
-    Matrix Xsmoothed = applyFivePointSavitzkyGolayFilter(Xsorted);
-    Matrix ysmoothed = applyFivePointSavitzkyGolayFilter(ysorted);
-
-    System.out.println("ysmoothed = \n" + ysmoothed);
-
-    return null;
   }
 
   private Matrix applyFivePointSavitzkyGolayFilter(Matrix matrix) {
@@ -135,11 +72,57 @@ public class YGradientGLSW extends AbstractSingleResponsePLS {
     }
 
     return res;
-
   }
 
   @Override
-  protected Matrix doPerformPredictions(Matrix predictors) throws Exception {
+  protected Matrix getCovarianceMatrix(Matrix X, Matrix y) {
+    double[] yVals = y.toRawCopy1D();
+    int[] sortedRowIndices = IntStream
+      .range(0, yVals.length)
+      .boxed()
+      .sorted(Comparator.comparingDouble(o -> yVals[o]))
+      .mapToInt(i -> i)
+      .toArray();
+
+    // Increasing/Decreasing??????
+    int[] allCols = IntStream.range(0, X.numColumns()).toArray();
+    Matrix Xsorted = X.getSubMatrix(sortedRowIndices, allCols);
+    Matrix ysorted = y.getSubMatrix(sortedRowIndices, new int[]{0});
+
+
+    // Apply 5-Point Savitzky–Golay filter
+    Matrix Xsmoothed = applyFivePointSavitzkyGolayFilter(Xsorted);
+    Matrix ysmoothed = applyFivePointSavitzkyGolayFilter(ysorted);
+    System.out.println("ysorted = " + ysorted);
+    System.out.println("ysmoothed = \n" + ysmoothed);
+
+    double ysmoothedMean = ysmoothed.mean(-1).asDouble();
+    double syd = ysmoothed.sub(ysmoothedMean).powElementwise(2).sum(-1).div(ysmoothed.numRows() - 1).asDouble();
+
+    // Reweighting matrix
+    Matrix W = MatrixFactory.zeros(y.numRows(), y.numRows());
+    for (int i = 0; i < ysmoothed.numRows(); i++) {
+      double ydi = ysmoothed.get(i, 0);
+      W.set(i, i, Math.pow(2, -1 * ydi / syd));
+    }
+
+    // Covariance Matrix
+    Matrix C = Xsmoothed.t().mul(W.mul(W)).mul(Xsmoothed);
+    return C;
+  }
+
+  /**
+   * Hook method for checking the data before training.
+   *
+   * @param x1 first sample set
+   * @param x2 second sample set
+   * @return null if successful, otherwise error message
+   */
+  protected String check(Matrix x1, Matrix x2) {
+    if (x1 == null)
+      return "No x1 matrix provided!";
+    if (x2 == null)
+      return "No x2 matrix provided!";
     return null;
   }
 }
