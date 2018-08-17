df7f755206a6d80a822b445d14cca66fc4d94379
Steven Lang <slang03@students.uni-mainz.de>
2018-08-17 11:01:49 +0200

    Add ResponseFilter interface

---
+++ src/main/java/com/github/waikatodatamining/matrix/algorithm/api/ResponseFilter.java
@@ -0,0 +1,24 @@
+package com.github.waikatodatamining.matrix.algorithm.api;
+
+import com.github.waikatodatamining.matrix.core.Matrix;
+
+/**
+ * Filter API that exposes a method
+ * {@link ResponseFilter#transformResponse(Matrix)}
+ * which takes a matrix and returns a matrix based on this filter's
+ * transformation rules.
+ *
+ * @author Steven Lang
+ */
+public interface ResponseFilter {
+
+  /**
+   * Transform a given matrix into another matrix based on the filter's
+   * implementation.
+   *
+   * @param response Input matrix
+   * @return Transformed matrix
+   * @throws Exception Transformation was not successful.
+   */
+  Matrix transformResponse(Matrix response) throws Exception;
+}
