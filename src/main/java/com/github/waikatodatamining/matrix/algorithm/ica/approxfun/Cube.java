acf0cd543d2e5cc2f473a49e0569b73976f65e0a
Steven Lang <steven.lang.mz@gmail.com>
2018-09-06 11:59:44 +0200

    Add FastICA implementation and test

---
+++ src/main/java/com/github/waikatodatamining/matrix/algorithm/ica/approxfun/Cube.java
@@ -0,0 +1,19 @@
+package com.github.waikatodatamining.matrix.algorithm.ica.approxfun;
+
+import com.github.waikatodatamining.matrix.core.Matrix;
+import com.github.waikatodatamining.matrix.core.Tuple;
+
+/**
+ * Cubic Negative Entropy Approximation Function.
+ *
+ * @author Steven Lang
+ */
+public class Cube implements NegEntropyApproximationFunction {
+
+  @Override
+  public Tuple<Matrix, Matrix> apply(Matrix x) {
+    Matrix gx = x.powElementwise(3);
+    Matrix g_x = x.powElementwise(2).mul(3).mean(1);
+    return new Tuple<>(gx, g_x);
+  }
+}
